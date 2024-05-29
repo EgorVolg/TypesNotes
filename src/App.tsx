6dc75c6bca3ac49340d493d28bdbe37995dedbdb
Valery <duna9588@gmail.com>
2024-05-29 18:38:24 +0300

    created ITodo for App
    created interface ITodo, created states, created basic structur

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useState } from "react";
+import { ITodo } from "./types/data";
 
-export const App = () => {
+export const App: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const addTodo = () => {
+    setTodos([...todos, {
+        title: value,
+        id: Date.now(),
+        isCompleted: false,
+    }])
+  };
+
   return (
-    <div>Hello world! Im typescript!</div>
-  )
-}
+    <div>
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+    </div>
+  );
+};
